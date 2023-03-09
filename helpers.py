082243663f53863e5a1aa72c7c727cd63e55c29e
Muneeb Ahmad <muneeb.gandapur@gmail.com>
2023-03-09 23:46:07 +0100

    chore: Save latest code state

--- helpers.py
+++ helpers.py
@@ -1,9 +1,14 @@
 import logging
+from typing import Any
+
 from paramiko import SSHClient, AutoAddPolicy, RSAKey
 from paramiko.ssh_exception import AuthenticationException, SSHException
 import os
 import socket
 import yaml
+import json
+import random
+import string
 from rich.table import Table
 from rich import box
 from rich.console import Console
@@ -13,6 +18,8 @@ import time
 import docker
 from docker import errors, DockerClient
 from ipaddress import ip_address, IPv6Address
+import requests
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
 logging.getLogger("paramiko").setLevel(logging.ERROR)
@@ -48,9 +55,9 @@ class OutputInfo(object):
         self.console = Console()
 
     def output_details_table(self, title: str, table_content: list) -> None:
-        table = Table(title=title, title_justify="center", box=box.HEAVY, show_lines=True)
+        table = Table(title=title, title_justify="left", box=box.HEAVY, show_lines=True, expand=True)
         for column in table_content[0].keys():
-            table.add_column(column, justify="center", style="cyan")
+            table.add_column(column, justify="center", style="cyan", no_wrap=True if column == "Addresses" else False)
         for row in table_content:
             table.add_row(*row.values())
         self.console.print(table)
@@ -89,27 +96,165 @@ class Utilities(object):
     def resolve_hostname(name) -> str:
         return socket.gethostbyname(name)
 
+    @staticmethod
+    def generate_string(length: int) -> str:
+        return ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=length))
+
+    @staticmethod
+    def request_api(api_url: str, path: str, token: str, method: str = "GET", body: dict = None, timeout: int = 10) -> tuple[int, Any]:
+        try:
+            assert token, f"API token is not set! It must be provided to connect to the '{api_url}' API."
+            response = {}
+            supported_methods = ["GET", "DELETE", "POST"]
+            method = method.upper()
+            assert method in supported_methods, f"Invalid API method '{method}' request. " \
+                                                f"Supported methods are {','.join(supported_methods)}."
+            headers = {
+                "Accept": "application/json",
+                "Authorization": f"Bearer {token}"
+            }
+            if method == "GET":
+                response = requests.get(f"{api_url}/{path}", headers=headers, timeout=timeout)
+            elif method == "DELETE":
+                response = requests.delete(f"{api_url}/{path}", headers=headers, timeout=timeout)
+            elif method == "POST":
+                assert body, f"Body/data is required for {method} API request."
+                response = requests.post(f"{api_url}/{path}", headers=headers, timeout=timeout, json=body)
+            response_status = response.status_code
+            try:
+                response_json = response.json()
+            except json.decoder.JSONDecodeError:
+                logger.debug(f"No JSON data received from the '{api_url}' API")
+                response_json = {}
+            return response_status, response_json
+        except (AssertionError, requests.exceptions.RequestException) as err:
+            logger.debug(err)
+            raise Exception(f"Failed to make a request to '{api_url}' API.")
+
 
 class Docker(object):
     @Utilities.time_it
     def __init__(self, user: str, host: str) -> None:
         self.user = user
         self.host = host
+        self.url = f"ssh://{self.user}@{self.host}" if self.user and self.host else "unix://var/run/docker.sock"
         try:
             if self.host and self.user:
                 if not Utilities.is_valid_ip(self.host):
                     self.host = Utilities.resolve_hostname(self.host)
-                # assert Utilities.is_valid_ip(self.host)
                 elif Utilities.is_ipv6(self.host):
                     self.host = f"[{self.host}]"
-            self.client = DockerClient(base_url=f"ssh://{self.user}@{self.host}" if self.user and self.host else "unix://var/run/docker.sock")
+            self.client = DockerClient(base_url=self.url)
             logger.debug(f"Successfully connected to the '{host}' Docker API over SSH using '{user}' user")
         except AssertionError as err:
             raise Exception(f"Invalid host '{host}' is provided that is neither IPv4 or IPv6")
-        except Exception as err:
-            logger.debug(f"Docker API connection exception: {err}")
-            raise Exception(f"Failed to connect to '{host}' Docker API over SSH using '{user}' user")
+        except docker.errors.APIError as err:
+            raise Exception(f"Failed to connect to the Docker API ({self.url}). {err}.")
 
-    # @Utilities.time_it
-    # def run(self, ):
-    #     logger.debug(f"Starting a Docker container")
+    @Utilities.time_it
+    def container_exists(self, name: str) -> bool:
+        try:
+            self.client.containers.get(name)
+            return True
+        except docker.errors.NotFound:
+            return False
+        except docker.errors.APIError as err:
+            logger.debug(f"Failed to connect to the Docker API ({self.url}). {err}.")
+            raise Exception(f"Failed to check '{name}' container status")
+
+    @Utilities.time_it
+    def exec_command(self, container: str, cmd: str) -> None:
+        try:
+            logger.debug(f"Executing the command '{cmd}' inside a '{container}' container")
+            return self.client.containers.get(container).exec_run(cmd)
+        except docker.errors.APIError as err:
+            logger.debug(f"Failed to connect to '{self.host}' Docker API ({self.url}). {err}")
+            raise Exception(f"Failed to execute the command '{cmd}' inside a '{container}' container")
+
+    @Utilities.time_it
+    def start_container(self, name: str) -> None:
+        try:
+            logger.debug(f"Starting/restarting the '{name}' container")
+            return self.client.containers.get(name).start()
+        except docker.errors.APIError as err:
+            logger.debug(f"Failed to connect to '{self.host}' Docker API ({self.url}). {err}")
+            raise Exception(f"Failed to start the '{name}' container")
+
+    @Utilities.time_it
+    def check_container_status(self, name: str) -> bool:
+        try:
+            if self.client.containers.get(name).status == "running":
+                logger.debug(f"'{name}' container is in 'running' state")
+                return True
+            logger.debug(f"'{name}' container is in 'exited' state")
+            return False
+        except docker.errors.APIError as err:
+            logger.debug(f"Failed to connect to '{self.host}' Docker API ({self.url}). {err}")
+            raise Exception(f"Failed to check the '{name}' container status")
+
+    @Utilities.time_it
+    def delete_container(self, name: str) -> None:
+        try:
+            self.client.containers.get(name).stop()
+            self.client.containers.get(name).remove()
+            logger.debug(f"'{name}' container is successfully deleted")
+        except docker.errors.APIError as err:
+            logger.debug(f"Failed to connect to '{self.host}' Docker API ({self.url}). {err}")
+            raise Exception(f"Failed to delete the '{name}' container")
+
+    @Utilities.time_it
+    def volume_exists(self, name: str) -> bool:
+        try:
+            self.client.volumes.get(name)
+            return True
+        except docker.errors.NotFound:
+            return False
+        except docker.errors.APIError as err:
+            logger.debug(f"Failed to connect to '{self.host}' Docker API ({self.url}). {err}.")
+            raise Exception(f"Failed to check '{name}' container volume status")
+
+    @Utilities.time_it
+    def create_volume(self, name: str, driver: str = "local") -> None:
+        try:
+            assert driver in ["local"], f"Docker storage driver '{driver}' is invalid. Only supported driver is 'local."
+            self.client.volumes.create(
+                name=name,
+                driver=driver
+            )
+        except docker.errors.APIError as err:
+            logger.debug(f"Failed to connect to '{self.host}' Docker API ({self.url}). {err}")
+            raise Exception(f"Failed to create the container volume '{name}' using '{driver}' storage driver")
+
+    @Utilities.time_it
+    def delete_volume(self, name: str) -> None:
+        try:
+            self.client.volumes.get(name).remove()
+            logger.debug(f"'{name}' container volume is successfully deleted")
+        except docker.errors.APIError as err:
+            logger.debug(f"Failed to connect to '{self.host}' Docker API ({self.url}). {err}")
+            raise Exception(f"Failed to delete the container volume '{name}'")
+
+    @Utilities.time_it
+    def fetch_host_interface_ips(self, interface: str) -> list:
+        try:
+            interfaces = []
+            interface_dump = json.loads(self.client.containers.run(
+                name="helper-interfaces",
+                image="centos",
+                command=f"ip -json addr show {interface}",
+                privileged=True,
+                detach=True,
+                network_mode="host",
+            ).logs())
+            for i in interface_dump[0]["addr_info"]:
+                if i["scope"] == "global":
+                    interfaces.append(i["local"])
+            self.delete_container("helper-interfaces")
+            logger.debug(f"Container host interface '{interface}' IPs [{','.join(interfaces)}] are successfully fetched")
+            return interfaces
+        except docker.errors.APIError as err:
+            logger.debug(f"Failed to connect to '{self.host}' Docker API ({self.url}). {err}")
+            raise Exception(f"Failed to fetch the container host interface '{interface}' IPs")
+        except json.decoder.JSONDecodeError:
+            logger.debug(f"Container host interface '{interface}' doesn't exist on the host")
+            raise Exception(f"Failed to fetch the container host interface '{interface}' IPs")
--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import logging.config
 import coloredlogs
 # from transit_services import TransitServices
 from helpers import Utilities, Docker
-from operations import Tailscale
+from operations import Tailscale, K3s
 
 
 @Utilities.time_it
@@ -121,25 +121,100 @@ def main():
             default=None,
             help="Tailscale API key to use for connection."
         )
+        k3s = subparsers.add_parser("k3s", help="Initialize/Join/Leave K3s cluster")
+        k3s.add_argument(
+            "operation",
+            nargs="+",
+            choices=[
+                "init",
+                "join",
+                "leave",
+                "status",
+                "config"
+            ],
+            help="K3s Operations.\n"
+                 "init: Initialize a K3s cluster.\n"
+                 "join: Join a K3s cluster as master or worker node.\n"
+                 "leave: Leave a K3s cluster.\n"
+                 "status: Print out K3s cluster status/details.\n"
+                 "status: Print out K3s cluster kubeconfig.\n"
+        )
+        k3s.add_argument(
+            "--k3s-cluster-cidrs",
+            default="10.42.0.0/16,2001:cafe:42:0::/56",
+            help="K3s cluster subnets/CIDRs that will be used for K3s Pods network."
+        )
+        k3s.add_argument(
+            "--k3s-service-cidrs",
+            default="10.43.0.0/16,2001:cafe:42:1::/112",
+            help="K3s service subnets/CIDRs that will be used for K3s services network."
+        )
+        k3s.add_argument(
+            "--k3s-additional-flags",
+            nargs="+",
+            default=[
+                "--disable servicelb",
+                "--kubelet-arg=node-ip=::"
+            ],
+            help="K3s tool additional flags/inputs to pass."
+        )
+        k3s.add_argument(
+            "--k3s-node-role",
+            default=None,
+            help="Node role in K3s cluster.",
+            choices=[
+                "server",
+                "agent"
+            ]
+        )
+        k3s.add_argument(
+            "--k3s-token",
+            default=None,
+            help="K3s cluster token."
+        )
+        k3s.add_argument(
+            "--k3s-api",
+            default=None,
+            help="K3s cluster API URL."
+        )
+        k3s.add_argument(
+            "--k3s-ipv6-only",
+            default=False,
+            help="K3s cluster API URL with IPv6 only."
+        )
 
         args = parser.parse_args()
         if args.lab == "tailscale":
-            tailscale = Tailscale(
+            tailscale_client = Tailscale(
                 org=args.tailscale_org,
                 host=args.ssh_ip,
                 username=args.ssh_username,
                 authkey=args.tailscale_authkey,
-                apikey=args.tailscale_apikey)
+                apikey=args.tailscale_apikey
+            )
             if "connect" in args.operation:
-                tailscale.connect()
+                tailscale_client.connect()
             elif "disconnect" in args.operation:
-                tailscale.disconnect()
+                tailscale_client.disconnect()
             elif "status" in args.operation:
-                tailscale.print_status()
+                tailscale_client.print_status()
+        elif args.lab == "k3s":
+            k3s_client = K3s(
+                host=args.ssh_ip,
+                username=args.ssh_username
+            )
+            if "init" in args.operation:
+                k3s_client.create(args.k3s_cluster_cidrs, args.k3s_service_cidrs)
+            elif "join" in args.operation:
+                k3s_client.join(args.k3s_node_role, args.k3s_api, args.k3s_token)
+            elif "leave" in args.operation:
+                k3s_client.leave()
+            elif "config" in args.operation:
+                k3s_client.get_kubeconfig(ipv6=args.k3s_ipv6_only)
     except Exception as err:
         logger.error("Oh shoot!")
         logger.error(err)
-        # raise
+        raise
 
 
 if __name__ == '__main__':
