dce9c1e2f7a549abd0fb319ee5eaddfddf8051d3
Muneeb Ahmad <muneeb.gandapur@gmail.com>
2023-07-24 23:12:45 +0200

    feat(network): Add security resources deployment

---
+++ terraform/apps/security.tf
@@ -0,0 +1,22 @@
+resource "kubernetes_namespace" "security" {
+  metadata {
+    name = "security"
+  }
+}
+
+moved {
+  from = module.cert-manager
+  to = module.cert_manager
+}
+
+module "cert_manager" {
+  source = "./modules/cert-manager"
+  namespace = kubernetes_namespace.security.metadata[0].name
+  domain_email = "muneebahmad22@live.com"
+  group_name = "acme.dera.ovh"
+  ovh_app_key = var.cert_manager_ovh_app_key
+  ovh_app_secret = var.cert_manager_ovh_app_secret
+  ovh_consumer_key = var.cert_manager_ovh_consumer_key
+  ingress_class = "nginx"
+  depends_on = [kubernetes_namespace.security]
+}
