021fa56743c915628a0b456c06c30cb5fb4bc569
Muneeb Ahmad <muneeb.gandapur@gmail.com>
2023-07-24 23:13:34 +0200

    feat(network): Add dns resources deployment

---
+++ terraform/apps/dns.tf
@@ -0,0 +1,33 @@
+resource "kubernetes_namespace" "dns" {
+  metadata {
+    name = "dns"
+  }
+}
+
+module "pihole" {
+  source = "./modules/pihole"
+  namespace = "dns"
+  expose = true
+  domains = [
+    "pihole.dera.ovh"
+  ]
+  password = var.pihole_password
+  ingress_class = "nginx"
+  issuer = module.cert_manager.issuer
+  publish = true
+  depends_on = [
+    kubernetes_namespace.dns,
+    module.cert_manager
+  ]
+}
+
+module "external_dns" {
+  source = "./modules/external-dns"
+  namespace = "dns"
+  pihole_server = "http://pihole-web.dns.svc.cluster.local"
+  pihole_password = var.pihole_password
+  depends_on = [
+    kubernetes_namespace.dns,
+    module.pihole
+  ]
+}
