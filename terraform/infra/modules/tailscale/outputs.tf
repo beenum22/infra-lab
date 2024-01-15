6a3aa601ccd457d2ee339e7bf57442b081c20e3c
Muneeb Ahmad <muneeb.gandapur@gmail.com>
2024-01-15 21:59:10 +0100

    chore: Cleanup redundant files and directories

--- terraform/infra/modules/tailscale/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ipv4_address" {
-  value = data.tailscale_device.device.addresses[0]
-}
-
-output "ipv6_address" {
-  value = data.tailscale_device.device.addresses[1]
-}
--- terraform/infra/network.tf
+++
@@ -1,6 +0,0 @@
-module "oracle_vcn" {
-  source = "./modules/oci_vcn"
-  compartment_id = var.compartment_id
-  name = "lab"
-  enable_ssh = true
-}
