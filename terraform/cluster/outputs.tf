3f482b9913b925991c0d180774aeb351a2d6bf31
Muneeb Ahmad <muneeb.gandapur@gmail.com>
2024-01-15 21:59:10 +0100

    chore: Cleanup redundant files and directories

--- terraform/cluster/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "k3s_node_cidrs" {
-  value = {for node, outputs in merge(module.lab_k3s_init, module.lab_k3s) : node => outputs["node_cidrs"]}
-}
-
-output "k3s_kubeconfig" {
-  value = module.lab_k3s_init["lab-k3s-0"].kubeconfig
-  sensitive = true
-}
