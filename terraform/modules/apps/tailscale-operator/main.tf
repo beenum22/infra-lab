1ea370942722f0ee82cb4ccda26c658c7543de17
Muneeb Ahmad <muneeb.gandapur@gmail.com>
2024-03-21 12:18:11 +0100

    feat(apps): Use official Tailscale operator chart

--- terraform/modules/apps/tailscale-operator/main.tf
+++ terraform/modules/apps/tailscale-operator/main.tf
@@ -1,21 +1,8 @@
-#locals {
-#  ingress_annotations = {
-#    "cert-manager\\.io/cluster-issuer" = var.issuer
-#    "kubernetes\\.io/ingress\\.class" = var.ingress_class
-#    "external-dns\\.alpha\\.kubernetes\\.io/internal-hostname" = replace(join("\\,", var.domains), ".", "\\.")
-#    "external-dns\\.alpha\\.kubernetes\\.io/target" = var.ingress_hostname
-#    "hajimari\\.io/enable" = var.publish
-#    "hajimari\\.io/icon" = "https://upload.wikimedia.org/wikipedia/commons/0/00/Pi-hole_Logo.png"
-#    "hajimari\\.io/appName" = "pihole"
-#    "hajimari\\.io/group" = "Cluster"
-#    "hajimari\\.io/url" = "https://${var.domains[0]}/admin"
-#    "hajimari\\.io/info" = "DNS Server with Adblocker"
-#  }
-#}
-
-resource "helm_release" "pihole" {
+resource "helm_release" "this" {
   name       = var.name
-  chart      = "${path.module}/chart"
+  repository = var.chart_url
+  chart      = var.chart_name
+  version    = var.chart_version
   namespace   = var.namespace
   set {
     name  = "oauth.clientId"
