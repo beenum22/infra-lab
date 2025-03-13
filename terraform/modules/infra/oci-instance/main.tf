71d2eeef12f3bb48b3788c3f70db3852ddedd303
Muneeb Ahmad <muneeb.gandapur@gmail.com>
2025-03-14 03:28:54 +0500

    fix: Use correct oci provider versions

--- terraform/modules/infra/oci-instance/main.tf
+++ terraform/modules/infra/oci-instance/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     oci = {
       source = "oracle/oci"
-      version = "4.111.0"
+      version = ">= 4.67.3, < 7.0.0"
     }
     template = {
       source = "hashicorp/template"
