48b12480390f7cc117a7a3747bee50c71f10f741
Muneeb Ahmad <muneeb.gandapur@gmail.com>
2025-04-04 09:59:37 +0200

    refactor(network): Deploy subnet router as Helm chart

--- terraform/modules/apps/tailscale-router/variables.tf
+++ terraform/modules/apps/tailscale-router/variables.tf
@@ -1,21 +1,10 @@
-variable "kubeconfig" {
-  type = string
-  default = "~/.kube/config"
-}
-
-variable "annotations" {
-  type = map(string)
-  default = null
-}
-
-variable "labels" {
-  type = map(string)
-  default = null
-}
-
 variable "name" {
   type = string
   default = "tailscale-subnet-router"
+}
+
+variable "hostname" {
+  type = string
 }
 
 variable "namespace" {
@@ -23,40 +12,6 @@ variable "namespace" {
   default = "tailscale"
 }
 
-variable "replicas" {
-  type = number
-  default = 1
-}
-
-variable "authkey" {
-  type = string
-}
-
-variable "image" {
-  type = string
-  default = "tailscale/tailscale"
-}
-
-variable "tag" {
-  type = string
-  default = "latest"
-}
-
 variable "routes" {
   type = list(string)
-}
-
-variable "extra_args" {
-  type = list(string)
-  default = []
-}
-
-variable "mtu" {
-  type = string
-  default = "1280"
-}
-
-variable "userspace_mode" {
-  type = bool
-  default = true
 }
